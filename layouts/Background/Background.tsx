6be0d41c64a5b69cb0604de24a6f74df6b9ecf03
Anton <antklim89@mail.ru>
2021-10-23 16:44:41 +0300

    update to next Image

--- layouts/Background/Background.tsx
+++ layouts/Background/Background.tsx
@@ -1,14 +1,20 @@
+import Image from 'next/image';
 import { FC } from 'react';
 
+import backgroundImage from './bg.jpg';
 import style from './style.module.scss';
-
-import bg from './bg.jpg';
 
 
 const Background: FC = () => {
     return (
         <div className={style.Background}>
-            <img alt="bg" className={style.img} src={bg.src} />
+            <Image
+                alt="background"
+                {...backgroundImage}
+                className={style.img}
+                layout="responsive"
+                placeholder="blur"
+            />
         </div>
     );
 };
