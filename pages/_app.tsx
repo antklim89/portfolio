51f7673cfe98ce2f0d5d9ea663d47647fae9eaef
Anton <antklim89@mail.ru>
2021-10-23 19:07:36 +0300

    add montserrat fonts and normolize.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
 import { appWithTranslation } from 'next-i18next';
 import { AppProps } from 'next/app';
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/400-italic.css';
+import '@fontsource/montserrat/700.css';
+import '@fontsource/montserrat/700-italic.css';
+import 'normalize.css';
 
 import Layout from '~/layouts/Layout';
 
