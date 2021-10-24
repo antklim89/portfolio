ab9d50c8eb7e177a1073a50d89edf1707837e27e
Anton <31469798+antklim89@users.noreply.github.com>
2021-10-24 19:18:47 +0000

    remove div

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,24 +2,20 @@ import type { NextPage, GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import Seo from '~/components/Seo';
-import Intro from '~/layouts/Intro';
 import ProjectsList from '~/layouts/ProjectsList';
-import { introSchema } from '~/schemas/intro';
 import { projectSchema } from '~/schemas/project';
-import type { IntroType, ProjectType } from '~/types';
-import { loadManyFiles, getLocale, loadOneFile } from '~/utils/server';
+import type { ProjectType } from '~/types';
+import { loadManyFiles, getLocale } from '~/utils/server';
 
 
 interface Props {
-    intro: IntroType
     projects: ProjectType[]
 }
 
-const Home: NextPage<Props> = ({ intro, projects }) => {
+const Home: NextPage<Props> = ({ projects }) => {
     return (
         <>
             <Seo />
-            <Intro {...intro} />
             <ProjectsList projects={projects} />
         </>
     );
@@ -30,16 +26,11 @@ export default Home;
 export const getStaticProps: GetStaticProps<Props> = async ({ locale = 'en' }) => {
     const localisation = await serverSideTranslations(locale, ['common', 'projects-list']);
 
-
-    const introData = await loadOneFile('intro/index.json');
-    const intro = getLocale(introData, introSchema, locale);
-
     const projectsData = await loadManyFiles('projects');
     const projects = projectsData.map((project) => getLocale(project, projectSchema, locale));
 
     return {
         props: {
-            intro,
             projects,
             ...localisation,
         },
