78b093669292518b5ea76336297d27a45b607b2e
Anton <antklim89@mail.ru>
2021-10-28 19:39:53 +0300

    install next 12 and sharp

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
-    images: { domains: ['localhost', '192.168.90.19'] },
+    images: {
+        minimumCacheTTL: 60 * 60 * 60 * 24,
+        domains: ['localhost', '192.168.90.19'],
+    },
     i18n,
     webpack: (config) => {
         config.resolve.fallback = { fs: false, path: false };
