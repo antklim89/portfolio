9ea63d5bc82088ce75f6c975db33c75b68307715
Anton <31469798+antklim89@users.noreply.github.com>
2021-10-28 19:11:38 +0000

    update Project component

--- layouts/Project/Project.tsx
+++ layouts/Project/Project.tsx
@@ -2,13 +2,18 @@ import { FC } from 'react';
 
 import style from './style.module.scss';
 
+import Tags from '~/components/Tags';
 import { ProjectType } from '~/types';
 
 
-const Project: FC<ProjectType> = ({ technologies, title, image, link, body }) => {
+const Project: FC<ProjectType> = ({ technologies, title, body }) => {
     return (
         <section className={style.Project}>
-            {body}
+            <h1 className="title">{title}</h1>
+            <Tags tags={technologies} />
+            <div className={style.Project__body}>
+                {body}
+            </div>
         </section>
     );
 };
