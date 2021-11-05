1cbaa4151c4821f4923110242263bc671b92362d
Anton <antklim89@mail.ru>
2021-11-05 21:12:12 +0300

    fix URL

--- next.config.js
+++ next.config.js
@@ -4,7 +4,11 @@ module.exports = {
     reactStrictMode: true,
     images: {
         minimumCacheTTL: 60 * 60 * 60 * 24,
-        domains: ['localhost', '192.168.90.19', new URL(process.env.URL).host],
+        domains: [
+            '192.168.90.19',
+            'localhost',
+            new URL(process.env.URL).host,
+        ],
     },
     i18n: {
         defaultLocale: 'en',
