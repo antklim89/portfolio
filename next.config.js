939cc5646f52c5264c0ca232a2903575fe008c41
Anton <antklim89@mail.ru>
2021-11-05 20:01:29 +0300

    add netlify URL host

--- next.config.js
+++ next.config.js
@@ -4,15 +4,11 @@ module.exports = {
     reactStrictMode: true,
     images: {
         minimumCacheTTL: 60 * 60 * 60 * 24,
-        domains: ['localhost', '192.168.90.19'],
+        domains: ['localhost', '192.168.90.19', new URL(process.env.URL).host],
     },
     i18n: {
         defaultLocale: 'en',
         locales: ['en', 'ru'],
     },
-    // webpack: (config) => {
-    //     config.resolve.fallback = { fs: false, path: false };
-    //     return config;
-    // },
     react: { useSuspense: false },
 };
