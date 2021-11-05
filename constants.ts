d529f911de4b6645f650b4456b584ea80fcae8de
Anton <antklim89@mail.ru>
2021-11-05 19:49:12 +0300

    fix URL env

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 
-export const SITE_URL = process.env.URL || 'http://localhost:3000';
+export const SITE_URL = process.env.NEXT_PUBLIC_URL || 'http://localhost:3000';
 
 export const CREATOR = 'Me';
 export const DEFAULT_DESCRIPTION = 'Portfolio';
