7b79488974b9e16d1ac4b4dc4f7f880f0c2d852f
Anton <antklim89@mail.ru>
2021-11-09 22:31:14 +0300

    update

--- utils/server/getBlurData.tsx
+++ utils/server/getBlurData.tsx
@@ -2,22 +2,41 @@ import path from 'path';
 
 import sharp from 'sharp';
 
+import { BlurData } from '~/types';
 
-export function getBlurData<K extends string, T extends Record<K, string>>(
+
+type DataWithImage<K extends string> = Record<K, string>;
+
+
+export function getBlurData<K extends string, T extends DataWithImage<K>>(
+    data: T,
+    imageKey: K,
+): Promise<BlurData<T>>;
+
+
+export function getBlurData<K extends string, T extends DataWithImage<K>>(
     data: T[],
     imageKey: K,
-): Promise<(T & { blurData: string; })[]> {
+): Promise<BlurData<T>[]>;
 
-    return Promise.all(data.map(async (project) => {
+export function getBlurData<K extends string, T extends DataWithImage<K>>(
+    data: T | T[],
+    imageKey: K,
+): Promise<BlurData<T>> | Promise<BlurData<T>[]> {
+
+    const transform = async (project: T): Promise<BlurData<T>> => {
         const image = path.join(process.cwd(), 'public', project[imageKey]);
         const imageBuffer = await sharp(image)
             .resize(64, 64)
             .blur(5)
-            .jpeg()
+            .webp()
             .toBuffer();
 
-        const blurData = `data:image/jpeg;base64, ${imageBuffer.toString('base64')}`;
+        const blurData = `data:image/webp;base64, ${imageBuffer.toString('base64')}`;
 
         return { ...project, blurData };
-    }));
+    };
+
+    if (Array.isArray(data)) return Promise.all(data.map(transform));
+    return transform(data);
 }
