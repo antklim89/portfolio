2dec76069a4a7533c86d634ac4cfbbc08b4c5fa1
Anton <antklim89@mail.ru>
2021-11-10 22:15:26 +0300

    added top offset

--- layouts/Layout/Layout.tsx
+++ layouts/Layout/Layout.tsx
@@ -3,13 +3,17 @@ import { FC } from 'react';
 import style from './style.module.scss';
 
 import LeftSide from '~/layouts/LeftSide';
+import { cls } from '~/utils';
 
 
 const Layout: FC = ({ children }) => {
     return (
         <div className={style.Layout}>
             <LeftSide />
-            <main>{children}</main>
+            <main>
+                <div className={cls(style.topOffset, 'parallax')} />
+                { children }
+            </main>
         </div>
     );
 };
