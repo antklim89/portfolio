8ad1ed3401c56da773b38e49a02d3a8987e15285
Anton <antklim89@mail.ru>
2021-11-21 11:05:55 +0300

    ignore svg blurData

--- utils/server/getBlurData.tsx
+++ utils/server/getBlurData.tsx
@@ -8,6 +8,8 @@ import { BlurData } from '~/types';
 type DataWithImage<K extends string> = Record<K, string>;
 
 
+const basePlaceholder = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAEALAAAAAABAAEAAAICTAEAOw==';
+
 export function getBlurData<K extends string, T extends DataWithImage<K>>(
     data: T,
     imageKey: K,
@@ -23,8 +25,9 @@ export function getBlurData<K extends string, T extends DataWithImage<K>>(
     data: T | T[],
     imageKey: K,
 ): Promise<BlurData<T>> | Promise<BlurData<T>[]> {
-    const transform = async (project: T): Promise<BlurData<T>> => {
-        const imagePath = path.join(process.cwd(), 'public', project[imageKey]);
+    const transform = async (transformData: T): Promise<BlurData<T>> => {
+        const imagePath = path.join(process.cwd(), 'public', transformData[imageKey]);
+        if (imagePath.endsWith('.svg')) return { ...transformData, blurData: basePlaceholder };
         const imageJimp = await Jimp.read(imagePath);
         const blurData = await imageJimp
             .resize(64, 64)
@@ -32,7 +35,7 @@ export function getBlurData<K extends string, T extends DataWithImage<K>>(
             .blur(2)
             .getBase64Async(Jimp.MIME_JPEG);
 
-        return { ...project, blurData };
+        return { ...transformData, blurData };
     };
 
     if (Array.isArray(data)) return Promise.all(data.map(transform));
