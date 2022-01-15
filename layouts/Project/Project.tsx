d42f8ef1c492a9857b35ef7f7b3a0acfd33b3b59
Anton <antklim89@mail.ru>
2022-01-15 17:01:49 +0300

    added links

--- layouts/Project/Project.tsx
+++ layouts/Project/Project.tsx
@@ -1,4 +1,8 @@
+import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 import { FC } from 'react';
+import { DiGithubBadge } from 'react-icons/di/';
+import { MdPublic } from 'react-icons/md/';
 
 import style from './style.module.scss';
 
@@ -7,11 +11,29 @@ import Tags from '~/components/Tags';
 import { ProjectType } from '~/types';
 
 
-const Project: FC<ProjectType> = ({ technologies, title, body }) => {
+const Project: FC<ProjectType> = ({ technologies, title, body, link, github }) => {
+    const t = useTranslations();
+
     return (
         <section className={style.Project}>
-            <h1 className="title">{title}</h1>
-            <Tags tags={technologies} />
+            <div className={style.Project__head}>
+                <h1 className="title">{title}</h1>
+                <Tags tags={technologies} />
+                <div>
+                    <Link href={github}>
+                        <a rel="noopener noreferrer" target="_blank">
+                            <DiGithubBadge />
+                            {t('GitHub')}
+                        </a>
+                    </Link>
+                    <Link href={link}>
+                        <a rel="noopener noreferrer" target="_blank">
+                            <MdPublic />
+                            {t('Site')}
+                        </a>
+                    </Link>
+                </div>
+            </div>
             <div className={style.Project__body}>
                 <Markdown >
                     {body}
