12b6ea12b3615faf60656baf083398ba00673b99
Anton <antklim89@mail.ru>
2022-04-24 14:41:06 +0300

    updated logo

--- layouts/LeftSide/LeftSideLogo.tsx
+++ layouts/LeftSide/LeftSideLogo.tsx
@@ -1,6 +1,8 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { FC } from 'react';
 
+import logo from './portfolio-logo.svg';
 import style from './style.module.scss';
 
 import { cls } from '~/utils';
@@ -10,7 +12,7 @@ const LeftSideLogo: FC = () => {
     return (
         <Link href="/">
             <a className={cls('link', style.Logo)}>
-                PORTFOLIO
+                <Image {...logo} />
             </a>
         </Link>
     );
