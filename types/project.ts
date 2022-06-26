ec69c10f6b8427608cf263c784dab903ceb5c843
Anton <antklim89@mail.ru>
2022-06-26 20:03:14 +0300

    updated types

--- types/project.ts
+++ types/project.ts
@@ -1,8 +1,6 @@
 import { z } from 'zod';
 
-import { projectSchema, projectSchema } from '~/schemas/project';
+import { projectSchema } from '~/schemas/project';
 
-
-export type ProjectPreviewType = z.infer<typeof projectSchema>
 
 export type ProjectType = z.infer<typeof projectSchema>
--- utils/server/getProjects.ts
+++ utils/server/getProjects.ts
@@ -1,11 +1,11 @@
 import { projectSchema } from '~/schemas';
-import { ProjectPreviewType } from '~/types';
+import { ProjectType } from '~/types';
 
 import { getLocale } from './getLocale';
 import { loadManyFiles } from './loadManyFiles';
 
 
-export const getProjects = async (locale: string): Promise<ProjectPreviewType[]> => {
+export const getProjects = async (locale: string): Promise<ProjectType[]> => {
     const projectsData = await loadManyFiles('projects');
     const projectsLocales = projectsData.map((project) => getLocale(project, locale));
     const projects = await projectSchema.array().parseAsync(projectsLocales);
