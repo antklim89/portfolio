eaac638b74e853151f3361975fced92f700d2259
Anton <antklim89@mail.ru>
2023-01-30 06:27:18 +0300

    update head component

---
+++ app/[locale]/contacts/head.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+
+const Head: FC = () => {
+    return (
+        <>
+            <title>Contacts</title>
+        </>
+    );
+};
+
+export default Head;
--- app/head.tsx
+++ app/head.tsx
@@ -1,12 +1,28 @@
-import { FC } from 'react';
+import { getTranslation } from '~/utils';
+import { getAbout, getServerLocale } from '~/utils/server';
 
 
-const Head: FC = () => {
+const Head = async () => {
+    const locale = getServerLocale();
+    const { defaultTitle } = await getTranslation(locale);
+    const { text, creator, keywords } = await getAbout(locale);
+
     return (
         <>
-            <title />
             <meta content="width=device-width, initial-scale=1" name="viewport" />
             <link href="/favicon.ico" rel="icon" />
+            <title>{defaultTitle}</title>
+            <link href="/favicon.ico" rel="icon" />
+            <meta content={text} name="description" />
+            <meta content={keywords.join(', ')} name="keywords" />
+            <meta content={text} name="description" />
+            <meta content={defaultTitle} property="og:title" />
+            <meta content={text} property="og:description" />
+            <meta content="website" property="og:type" />
+            <meta content="summary" name="twitter:card" />
+            <meta content={creator} name="twitter:creator" />
+            <meta content={defaultTitle} name="twitter:title" />
+            <meta content={text} name="twitter:description" />
         </>
     );
 };
