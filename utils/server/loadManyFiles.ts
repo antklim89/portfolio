da800615674265a63c3104745d23c9ea8cb31f1c
Anton <antklim89@mail.ru>
2023-12-10 19:40:17 +0300

    chore: rename variables

--- utils/server/loadManyFiles.ts
+++ utils/server/loadManyFiles.ts
@@ -10,9 +10,9 @@ export async function loadManyFiles(filesFolder: string): Promise<unknown[]> {
 
     const projects = filesInFolder
         .filter((file) => (/\.json$/gi).test(file))
-        .map(async (projectFile) => {
-            const file = await fs.readFile(path.resolve(filesFolderPath, projectFile), 'utf-8');
-            const object = JSON.parse(file);
+        .map(async (fileName) => {
+            const fileContent = await fs.readFile(path.resolve(filesFolderPath, fileName), 'utf-8');
+            const object = JSON.parse(fileContent);
             return object;
         });
 
