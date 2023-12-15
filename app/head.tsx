c7720b40381b96926accc5c94dea6ba5e4e6b827
Anton <antklim89@mail.ru>
2023-12-15 21:02:54 +0300

    fix: change variables

--- app/head.tsx
+++ app/head.tsx
@@ -1,11 +1,10 @@
-import { getTranslation } from '~/utils';
-import { getAbout, getServerLocale } from '~/utils/server';
+import { getAbout, getServerLocale, getTranslation } from '~/utils/server';
 
 
 const Head = async () => {
     const locale = getServerLocale();
     const { defaultTitle } = await getTranslation(locale);
-    const { text, creator, keywords } = await getAbout(locale);
+    const { name, description, keywords } = await getAbout(locale);
 
     return (
         <>
@@ -13,16 +12,16 @@ const Head = async () => {
             <link href="/favicon.ico" rel="icon" />
             <title>{defaultTitle}</title>
             <link href="/favicon.ico" rel="icon" />
-            <meta content={text} name="description" />
+            <meta content={description} name="description" />
             <meta content={keywords.join(', ')} name="keywords" />
-            <meta content={text} name="description" />
+            <meta content={description} name="description" />
             <meta content={defaultTitle} property="og:title" />
-            <meta content={text} property="og:description" />
+            <meta content={description} property="og:description" />
             <meta content="website" property="og:type" />
             <meta content="summary" name="twitter:card" />
-            <meta content={creator} name="twitter:creator" />
+            <meta content={name} name="twitter:creator" />
             <meta content={defaultTitle} name="twitter:title" />
-            <meta content={text} name="twitter:description" />
+            <meta content={description} name="twitter:description" />
         </>
     );
 };
