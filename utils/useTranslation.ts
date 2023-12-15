e18558e376a9e481d8e3598153857b88698118db
Anton <antklim89@mail.ru>
2023-12-15 21:04:39 +0300

    chore: add useParams

--- utils/useTranslation.ts
+++ utils/useTranslation.ts
@@ -1,4 +1,4 @@
-import { useRouter, usePathname } from 'next/navigation';
+import { useRouter, usePathname, useParams } from 'next/navigation';
 import { useCallback, useContext } from 'react';
 
 import { TranslationContext } from '~/components/TranslationProvider';
@@ -13,11 +13,10 @@ const localeRegex = /^\/(\w\w)(.*)/i;
 export function useTranslation() {
     const { translation, locale } = useContext(TranslationContext);
     const { replace } = useRouter();
+    const { locale: oldLocale } = useParams();
     const pathname = usePathname() || '';
 
     const changeLocale = useCallback((newLocale: Locale) => {
-        const oldLocale = pathname.replace(localeRegex, '$1');
-
         if (isLocale(oldLocale) && isLocale(newLocale)) {
             const newPath = pathname.replace(localeRegex, `/${newLocale}$2`);
             setCookie('locale', newLocale);
