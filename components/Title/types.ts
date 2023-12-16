719bcf927625e478243cd6d27d5af7f5b1b631f1
Anton <antklim89@mail.ru>
2023-12-16 15:08:13 +0300

    chore: remove Tags and Title components

--- components/Title/types.ts
+++
@@ -1,9 +0,0 @@
-import { HTMLAttributes } from 'react';
-
-
-export interface TitleProps extends HTMLAttributes<HTMLHeadingElement> {
-     size?: 'md' | 'lg' | 'xl'
-     align?: 'center' | 'left'
-     as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
-     underscore?: boolean
-}
