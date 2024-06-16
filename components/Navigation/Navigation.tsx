c1a095a523919ded982a3ce29a8a0b02b286405b
Anton <antklim89prog@mail.ru>
2024-06-16 22:03:53 +0300

    update navigation

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,5 +1,5 @@
 import { ComponentProps } from 'react';
-import { FaBars } from 'react-icons/fa';
+import { FaHome } from 'react-icons/fa';
 
 import { Locale } from '~/types';
 import { cls } from '~/utils';
@@ -12,31 +12,31 @@ const Navigation = async ({ locale: currentLocale, className, ...props  }: { loc
     const t = await getTranslation(currentLocale);
 
     const links = [
-        { hash: '#', body: t.home },
+        { hash: '#', body: t.home, icon: <FaHome /> },
         { hash: '#projects', body: t.projects },
         { hash: '#technologies', body: t.technologies },
         { hash: '#contacts', body: t.contacts },
-    ];
+    ] as const;
 
     return (
         <section className={cls(style.Navigation, className)} {...props} >
             <div className='hide-lg'>
                 <ul>
                     {links.map(link => (
-                        <li key={link.body}><a href={link.hash}>{link.body}</a></li>
+                        <li key={link.body}>
+                            <a href={link.hash}>{link.body}</a>
+                        </li>
                     ))}
                 </ul>
             </div>
-            <div className={cls('show-lg', style.dropdown)}>
-                <div className='dropdown'>
-                    <div className={cls(style.toggle)} role="button" tabIndex={0}><FaBars /></div>
-                    <ul className="dropdown-content">
-                        {links.map(link => (
-                            <li key={link.body}><a href={link.hash}>{link.body}</a></li>
-
-                        ))}
-                    </ul>
-                </div>
+            <div className={cls('show-lg', style.topLinks)}>
+                <ul className="">
+                    {links.map(link => (
+                        <li key={link.body}>
+                            <a href={link.hash}>{'icon' in link ? link.icon : link.body}</a>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </section>
     );
