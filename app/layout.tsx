4a6f5e2b85dc5e134e024da5a24def6fd1d5a0fc
Anton <antklim89prog@mail.ru>
2024-06-30 16:02:18 +0300

    replace hed.tsx with generateMetadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,39 @@ import '@fontsource/montserrat/700.css';
 import '@fontsource/montserrat/700-italic.css';
 import '~/styles/main.scss';
 import '~/styles/properties.scss';
+import { Metadata } from 'next';
+
 import NetlifyIdentityWidget from '~/components/NetlifyIdentityWidget';
+import { getAbout, getServerLocale, getTranslation } from '~/utils/server';
+
+
+export async function generateMetadata(): Promise<Metadata> {
+    const locale = getServerLocale();
+    const { defaultTitle } = await getTranslation(locale);
+    const { name, description, keywords } = await getAbout(locale);
+
+    return {
+        manifest: '/manifest.json',
+        title: defaultTitle,
+        description,
+        keywords,
+        authors: [{ name }],
+        creator: name,
+        twitter: {
+            card: 'summary',
+            description,
+            title: defaultTitle,
+        },
+        openGraph: {
+            type: 'website',
+            description,
+            locale,
+            title: defaultTitle,
+            url: '/',
+            siteName: defaultTitle,
+        },
+    };
+}
 
 
 const RootLayout = async ({ children }: { children: React.ReactNode}) => {
