90f46afce44cc070e793c92f7fb4f4d1e908a6b4
Anton <antklim89@mail.ru>
2024-08-28 20:49:49 +0300

    chore: add translationPath

--- utils/server/getTranslation.ts
+++ utils/server/getTranslation.ts
@@ -3,7 +3,8 @@ import fs from 'fs-extra';
 import type { LocaleType, Translation } from '~/types';
 
 export async function getTranslation(locale: LocaleType): Promise<Translation> {
-    const translationContent = await fs.readFile(path.resolve(process.cwd(), `locales/${locale}.json`), 'utf-8');
+    const translationPath = path.resolve(process.cwd(), `locales/${locale}.json`);
+    const translationContent = await fs.readFile(translationPath, 'utf-8');
     const translation = JSON.parse(translationContent);
 
     return translation;
