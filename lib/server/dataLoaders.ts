ea2d571ab4153ac067856df92a91e611b3603901
Anton <=>
2024-09-05 16:33:33 +0300

    refactor: update loaders

--- lib/server/dataLoaders.ts
+++ lib/server/dataLoaders.ts
@@ -2,63 +2,60 @@ import path from 'node:path';
 import process from 'node:process';
 import fs from 'fs-extra';
 import { type ZodType, z } from 'zod';
+import { locales } from '@/lib/constants';
 import { aboutSchema, projectSchema, technologySchema } from '@/lib/schemas';
-import type { AboutType, ProjectType, TechnologyType } from '@/lib/types';
+import type { AboutType, LocaleType, ProjectType, TechnologyType } from '@/lib/types';
 
 
-export function mergeLocales<T extends ZodType<unknown>>(data: unknown, locale: string): z.infer<T> {
-  const record = z.record(z.record(z.unknown())).parse(data);
+const BASE_CONTENT_PATH = path.resolve(process.cwd(), 'public/content');
 
+const contentLocaleSchema = z.record(z.enum(locales as [typeof locales[number]]), z.record(z.unknown()));
+
+export function mergeLocales<T extends ZodType<unknown>>(data: unknown, locale: LocaleType): z.infer<T> {
+  const record = contentLocaleSchema.parse(data);
   return { ...record.en, ...record[locale] };
 }
 
-export async function getAbout(locale: string): Promise<AboutType> {
-  const about = await loadOneFile('about', 'index');
-  const aboutLocale = mergeLocales(about, locale);
-  const result = await aboutSchema.parseAsync(aboutLocale);
+export async function getAbout(locale: LocaleType): Promise<AboutType> {
+  const data = await loadOneFile('about/index');
+  const dataLocale = mergeLocales(data, locale);
+  const result = await aboutSchema.parseAsync(dataLocale);
 
   return result;
 }
 
-export async function getProjects(locale: string): Promise<ProjectType[]> {
-  const projectsData = await loadManyFiles('projects');
-  const projectsLocales = projectsData.map(project => mergeLocales(project, locale));
-  const projects = await projectSchema.array().parseAsync(projectsLocales);
-
-  return projects;
-}
-
-export async function getTechnologies(locale: string): Promise<TechnologyType[]> {
-  const technologies = await loadManyFiles('technologies');
-  const technologiesLocales = technologies.map(item => mergeLocales(item, locale));
-  const result = await technologySchema.array().parseAsync(technologiesLocales);
+export async function getProjects(locale: LocaleType): Promise<ProjectType[]> {
+  const data = await loadManyFiles('projects');
+  const dataLocales = data.map(project => mergeLocales(project, locale));
+  const result = await projectSchema.array().parseAsync(dataLocales);
 
   return result;
 }
 
+export async function getTechnologies(locale: LocaleType): Promise<TechnologyType[]> {
+  const data = await loadManyFiles('technologies');
+  const dataLocales = data.map(item => mergeLocales(item, locale));
+  const result = await technologySchema.array().parseAsync(dataLocales);
+
+  return result;
+}
 
 export async function loadManyFiles(filesFolder: string): Promise<unknown[]> {
-  const filesFolderPath = path.resolve(process.cwd(), 'public/content', filesFolder);
+  const filesPath = path.join(BASE_CONTENT_PATH, filesFolder);
+  const fileNames = await fs.readdir(filesPath);
 
-  const filesInFolder = await fs.readdir(filesFolderPath);
-
-  const projects = filesInFolder
-    .filter(file => /\.json$/i.test(file))
+  const result = fileNames
+    .filter(fileName => path.extname(fileName) === '.json')
     .map(async (fileName) => {
-      const fileContent = await fs.readFile(path.resolve(filesFolderPath, fileName), 'utf-8');
-      const object = JSON.parse(fileContent) as unknown;
-      return object;
+      const fileContent = await fs.readFile(path.join(filesPath, fileName), 'utf-8');
+      return JSON.parse(fileContent) as unknown;
     });
 
-  return Promise.all(projects);
+  return Promise.all(result);
 }
 
-
-export async function loadOneFile(fileDir: string, fileName: string): Promise<unknown> {
-  const filesFolderPath = path.resolve(process.cwd(), 'public/content', fileDir, `${fileName}.json`);
-
-  const file = await fs.readFile(filesFolderPath, 'utf-8');
-  const object = JSON.parse(file) as unknown;
-
-  return object;
+export async function loadOneFile(fileName: string): Promise<unknown> {
+  const filePath = path.join(BASE_CONTENT_PATH, `${fileName}.json`);
+  const fileContent = await fs.readFile(filePath, 'utf-8');
+  return JSON.parse(fileContent) as unknown;
 }
