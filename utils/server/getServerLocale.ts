8b93412b8b57e1d802a6b5c77e7b3e3db0f3f61c
Anton <=>
2024-09-05 12:25:31 +0300

    refactor: update getServerLocale with accept-language

--- utils/server/getServerLocale.ts
+++ utils/server/getServerLocale.ts
@@ -1,16 +1,16 @@
+import acceptLanguage from 'accept-language';
 import { cookies, headers } from 'next/headers';
 import { defaultLocale } from '~/constants';
 import type { LocaleType } from '~/types';
-import { isLocale } from '../checkLocale';
+import { isLocale } from '~/utils';
+
 
 export function getServerLocale(): LocaleType {
-    const cookiesLocale = cookies().get('locale')?.value;
-    if (isLocale(cookiesLocale)) return cookiesLocale;
+  const cookiesLocale = cookies().get('locale')?.value;
+  if (isLocale(cookiesLocale)) return cookiesLocale;
 
-    const headerLocale = headers()
-        .get('accept-language')
-        ?.replace(/.*,(.*);.*/i, '$1');
-    if (isLocale(headerLocale)) return headerLocale;
+  const headerLocale = acceptLanguage.get(headers().get('accept-language'));
+  if (isLocale(headerLocale)) return headerLocale;
 
-    return defaultLocale;
+  return defaultLocale;
 }
