f3b39048bc5f3676f94f67d33ee6b81939363125
Anton <antklim89@mail.ru>
2025-01-26 21:24:29 +0300

    feat: add payload actions

---
+++ lib/actions.ts
@@ -0,0 +1,93 @@
+import { cache } from 'react';
+import config from '@payload-config';
+import { getPayload } from 'payload';
+import { DEFAULT_BLUR_DATA } from '@/lib/constants';
+import type {
+  AboutType,
+  LocaleType,
+  ProjectType,
+  SeoType,
+  TechnologyType,
+} from '@/lib/types';
+import type { ProjectsMedia, TechnologiesMedia } from '@/payload-types';
+
+
+export const getSeo = cache(async (locale: LocaleType): Promise<SeoType> => {
+  const payload = await getPayload({ config });
+  const result = await payload.findGlobal({
+    slug: 'seo',
+    locale,
+  });
+
+  return {
+    id: result.id,
+    description: result.description,
+    author: result.author,
+    keywords: result.keywords,
+    title: result.title,
+  };
+});
+
+export const getAbout = cache(async (locale: LocaleType): Promise<AboutType> => {
+  const payload = await getPayload({ config });
+  const result = await payload.findGlobal({
+    slug: 'about',
+    locale,
+  });
+
+  return {
+    id: result.id,
+    description: result.description,
+    name: result.name,
+    profession: result.profession,
+    slogan: result.slogan,
+  };
+});
+
+export const getProjects = cache(async (locale: LocaleType): Promise<ProjectType[]> => {
+  const payload = await getPayload({ config });
+  const result = await payload.find({
+    collection: 'projects',
+    locale,
+    depth: 1,
+    pagination: false,
+  });
+
+  return result.docs.map((i) => {
+    const { blurDataURL, url } = i.image as ProjectsMedia;
+
+    return ({
+      id: i.id,
+      title: i.title,
+      body: i.body,
+      link: i.link,
+      github: i.github,
+      technologies: i.technologies,
+      imageUrl: url || DEFAULT_BLUR_DATA,
+      blurDataURL: blurDataURL || DEFAULT_BLUR_DATA,
+    });
+  });
+});
+
+export const getTechnologies = cache(async (locale: LocaleType): Promise<TechnologyType[]> => {
+  const payload = await getPayload({ config });
+  const result = await payload.find({
+    collection: 'technologies',
+    locale,
+    depth: 1,
+    pagination: false,
+  });
+
+  return result.docs.map((i) => {
+    const { blurDataURL, url } = i.image as TechnologiesMedia;
+
+    return ({
+      id: i.id,
+      body: i.body,
+      link: i.link,
+      title: i.title,
+      imageUrl: url || DEFAULT_BLUR_DATA,
+      blurDataURL: blurDataURL || DEFAULT_BLUR_DATA,
+    });
+  });
+});
