7ac518bbff5f2c84439b859872e3a810be46368b
Anton <antklim89@mail.ru>
2025-01-30 21:59:22 +0300

    refactor: remove cookie utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,13 +2,11 @@ import { defaultLocale, locales } from '@/lib/constants';
 import type { LocaleType } from '@/lib/types';
 
 
-// TODO: refactor translation utils
 export function isCorrectLocale(locale: unknown): locale is LocaleType {
   if (typeof locale !== 'string') return false;
   return locales.includes(locale as LocaleType);
 }
 
-// TODO: refactor translation utils
 export function getCorrectLocale(locale: unknown): LocaleType {
   return isCorrectLocale(locale) ? locale : defaultLocale;
 }
@@ -18,10 +16,4 @@ export function cls(...classes: Array<string | undefined | false | null>): strin
     if (typeof item === 'string' && item.trim().length > 0) return `${acc} ${item}`;
     return acc;
   }, '');
-}
-
-export function clearCookie(name: string): void {
-  if (typeof window !== 'undefined') {
-    document.cookie = `${name}=;path=/;expires=Thu, 01 Jan 1970 00:00:00 GMT;`;
-  }
 }
