d67e83ac4f2df2191c4dd3a1b5bb11d83041bd04
Anton <antklim89@mail.ru>
2025-01-31 20:10:59 +0300

    refactor: update getTranslation
    import locales

--- lib/utils.server.ts
+++ lib/utils.server.ts
@@ -1,29 +1,24 @@
 import { cookies as getCookies, headers as getHeaders } from 'next/headers';
-import path from 'node:path';
-import process from 'node:process';
+import { cache } from 'react';
 import acceptLanguage from 'accept-language';
-import fs from 'fs-extra';
 import { defaultLocale } from '@/lib/constants';
 import type { LocaleType, Translation } from '@/lib/types';
 import { isCorrectLocale } from '@/lib/utils';
 
 
-export async function getServerLocale(): Promise<LocaleType> {
+export const getServerLocale = cache(async (): Promise<LocaleType> => {
   const cookies = await getCookies();
-  const headers = await getHeaders();
   const cookiesLocale = cookies.get('locale')?.value;
   if (isCorrectLocale(cookiesLocale)) return cookiesLocale;
 
+  const headers = await getHeaders();
   const headerLocale = acceptLanguage.get(headers.get('accept-language'));
   if (isCorrectLocale(headerLocale)) return headerLocale;
 
   return defaultLocale;
-}
+});
 
-export async function getTranslation(locale: LocaleType): Promise<Translation> {
-  const translationPath = path.resolve(process.cwd(), `lib/locales/${locale}.json`);
-  const translationContent = await fs.readFile(translationPath, 'utf-8');
-  const translation = JSON.parse(translationContent) as Translation;
-
-  return translation;
-}
+export const getTranslation = cache(async (locale: LocaleType): Promise<Translation> => {
+  const translation = await import(`@/lib/locales/${locale}.json`) as Translation;
+  return { ...translation };
+});
