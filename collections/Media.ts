4023171c42dd334bfca7430ae6f36692c4acea74
Anton <antklim89@mail.ru>
2025-02-01 14:34:55 +0300

    feat: update blurDataURL generating

--- collections/Media.ts
+++ collections/Media.ts
@@ -1,18 +1,24 @@
 import { randomUUID } from 'node:crypto';
 import type { CollectionConfig } from 'payload';
 import sharp from 'sharp';
+import { DEFAULT_BLUR_DATA } from '@/lib/constants';
 
 
 export const Media = {
   slug: 'media',
   hooks: {
-    beforeOperation: [
-      async ({ req, operation }) => {
-        if ((operation === 'create' || operation === 'update') && req.file && req.data) {
+    beforeValidate: [
+      async ({ req, operation, data }) => {
+        if ((operation === 'create' || operation === 'update') && req.file) {
           const buffer = await sharp(req.file.data).resize({ width: 24 }).webp({ quality: 20 }).toBuffer();
           const blurDataURL = `data:image/webp;base64,${buffer.toString('base64')}`;
-          req.data.blurDataURL = blurDataURL;
-
+          return { ...data, blurDataURL };
+        }
+      },
+    ],
+    beforeOperation: [
+      async ({ req, operation }) => {
+        if ((operation === 'create' || operation === 'update') && req.file) {
           req.file.name = `${randomUUID()}-${req.file.name}`;
         }
       },
@@ -25,10 +31,14 @@ export const Media = {
     {
       type: 'text',
       name: 'blurDataURL',
-      admin: {
-        readOnly: true,
-        hidden: true,
-      },
+      required: true,
+      defaultValue: DEFAULT_BLUR_DATA,
+    },
+    {
+      type: 'text',
+      name: 'url',
+      required: true,
+      defaultValue: '/logo512.png',
     },
   ],
   upload: {
