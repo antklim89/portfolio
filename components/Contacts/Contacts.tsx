a931d291e286703520b9eebef94dcc59652f5515
Anton <antklim89@mail.ru>
2026-04-14 12:38:34 +0000

    feat: update Contacts

--- components/Contacts/Contacts.tsx
+++ components/Contacts/Contacts.tsx
@@ -1,6 +1,6 @@
 'use client';
-import type { ComponentProps, FormEventHandler } from 'react';
-import { useState, useTransition } from 'react';
+import type { ComponentProps, SubmitEvent } from 'react';
+import { useId, useState, useTransition } from 'react';
 
 import { useTranslation } from '@/hooks/useTranslation';
 import { cls } from '@/lib/utils';
@@ -8,12 +8,13 @@ import { submitContactsForm } from './Contacts.action';
 import style from './style.module.scss';
 
 function Contacts({ className, ...props }: ComponentProps<'section'>) {
+  const id = useId();
   const [status, setStatus] = useState<'success' | 'error' | null>(null);
   const [pending, startTransition] = useTransition();
 
   const { t } = useTranslation();
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = e => {
+  function handleSubmit(e: SubmitEvent<HTMLFormElement>) {
     e.preventDefault();
     startTransition(async () => {
       const form = e.currentTarget;
@@ -32,42 +33,45 @@ function Contacts({ className, ...props }: ComponentProps<'section'>) {
         setStatus('error');
       }
     });
-  };
+  }
 
   return (
     <section className={cls(style.Contacts, className)} {...props}>
-      <div>
-        <h3 className="title-lg">{t.contacts}</h3>
+      <h3 className="title-lg">{t.contacts}</h3>
 
-        {status === 'success' && <p className={cls(style.status, style.success)}>{t.contactSuccess}</p>}
-        {status === 'error' && <p className={cls(style.status, style.error)}>{t.contactError}</p>}
+      {status === 'success' && <p className={cls(style.status, style.success)}>{t.contactSuccess}</p>}
+      {status === 'error' && <p className={cls(style.status, style.error)}>{t.contactError}</p>}
 
-        <form className={style.form} name="contact" onSubmit={handleSubmit}>
-          <input name="form-name" type="hidden" value="contact" />
+      <form className={style.form} name="contact" onSubmit={handleSubmit}>
+        <input name="form-name" type="hidden" value="contact" />
 
-          <label className={style.input}>
-            {t.Name}:
-            <br />
-            <input required disabled={pending} maxLength={100} minLength={3} name="name" type="text" />
-          </label>
+        <div>
+          <label htmlFor={`${id}-name`}>{t.Name}:</label>
+          <input id={`${id}-name`} required disabled={pending} maxLength={100} minLength={3} name="name" type="text" />
+        </div>
 
-          <label className={style.input}>
-            {t.Subject}:
-            <br />
-            <input required disabled={pending} maxLength={100} minLength={3} name="subject" type="text" />
-          </label>
+        <div>
+          <label htmlFor={`${id}-subject`}>{t.Subject}:</label>
+          <input
+            id={`${id}-subject`}
+            required
+            disabled={pending}
+            maxLength={100}
+            minLength={3}
+            name="subject"
+            type="text"
+          />
+        </div>
 
-          <label className={style.input}>
-            {t.Message}:
-            <br />
-            <textarea required disabled={pending} maxLength={1000} minLength={3} name="text" rows={6} />
-          </label>
+        <div>
+          <label htmlFor={`${id}-text`}>{t.Message}:</label>
+          <textarea id={`${id}-text`} required disabled={pending} maxLength={1000} minLength={3} name="text" rows={6} />
+        </div>
 
-          <button disabled={pending} type="submit">
-            {t.Submit}
-          </button>
-        </form>
-      </div>
+        <button disabled={pending} type="submit">
+          {t.Submit}
+        </button>
+      </form>
     </section>
   );
 }
